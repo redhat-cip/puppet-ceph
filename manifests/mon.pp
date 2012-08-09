bc6785d9a6a5330bd0df8597b82fbd04da30d9c5
François Charlier <francois.charlier@enovance.com>
2012-08-09 20:06:24 +0200

    Fix permissions on mon data folder

--- manifests/mon.pp
+++ manifests/mon.pp
@@ -50,10 +50,9 @@ define ceph::mon (
 
   file { $mon_data_expanded:
     ensure  => directory,
-    recurse => true,
     owner   => 'root',
     group   => 0,
-    mode    => '0750',
+    mode    => '0755',
   }
 
 #FIXME: first step to make the monitor secret disappear from "ps" output ?
