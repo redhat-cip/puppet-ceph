021bec454a604967d31367c3ef766caa2c122209
François Charlier <francois.charlier@enovance.com>
2012-08-13 18:58:46 +0200

    Add a ceph::key resource
    It generates a keyring file from a secret & capabilities

---
+++ manifests/key.pp
@@ -0,0 +1,16 @@
+define ceph::key (
+  $secret       = undef,
+  $keyring_path = "/var/lib/ceph/tmp/${name}.keyring",
+  $capabilities = {},
+) {
+
+  #FIXME: inline-template to extract capabilities
+  $capabilities_str = ""
+
+  exec { "ceph-key-${name}":
+    command => "ceph-authtool ${keyring_path} --create-keyring --name=${name} --add-key='${secret}' ${capabilities_str}",
+    creates => $keyring_path,
+    require => Pacakge['ceph'],
+  }
+
+}
