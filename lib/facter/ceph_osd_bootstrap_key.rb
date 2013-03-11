e8242730bce92b049d5d6e1c63fe4fe1c1739013
François Charlier <francois.charlier@enovance.com>
2013-03-11 16:11:07 +0100

    Add blikd_uuid & ceph_osd_id facts

--- lib/facter/ceph_osd_bootstrap_key.rb
+++ lib/facter/ceph_osd_bootstrap_key.rb
@@ -7,13 +7,46 @@
 # Caveats:
 #
 
+require 'facter'
+
 ## ceph_osd_bootstrap_key
 ## Fact that gets the ceph key "client.bootstrap-osd"
-
-require 'facter'
 
 Facter.add(:ceph_osd_bootstrap_key) do
   setcode do
     Facter::Util::Resolution.exec("ceph auth get-key client.bootstrap-osd")
+  end
+end
+
+## blkid_uuid_#{device} / ceph_osd_id_#{device}
+## Facts that export partitions uuids & ceph osd id of device
+
+# Load the osds/uuids from ceph
+ceph_osds = Hash.new
+ceph_osd_dump = Facter::Util::Resolution.exec("ceph osd dump")
+if ceph_osd_dump
+  ceph_osd_dump.each do |line|
+    if line =~ /^osd\.(\d+).* ([a-f0-9\-]+)$/
+      ceph_osds[$2] = $1
+    end
+  end
+end
+
+Facter::Util::Resolution.exec("blkid").each do |line|
+  if line =~ /^\/dev\/(.+):.*UUID="([a-fA-F0-9\-]+)"/
+    device = $1
+    uuid = $2
+
+    Facter.add("blkid_uuid_#{device}") do
+      setcode do
+        uuid
+      end
+    end
+
+    Facter.add("ceph_osd_id_#{device}") do
+      setcode do
+        ceph_osds[uuid]
+      end
+    end
   end
 end
