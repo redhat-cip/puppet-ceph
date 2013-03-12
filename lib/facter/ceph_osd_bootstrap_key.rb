633516c8f55f2ba32baf2c3e54bdfa22aace8805
François Charlier <francois.charlier@enovance.com>
2013-03-12 17:01:30 +0100

    We changed this in the manifests before

--- lib/facter/ceph_osd_bootstrap_key.rb
+++ lib/facter/ceph_osd_bootstrap_key.rb
@@ -12,9 +12,9 @@ require 'facter'
 ## ceph_osd_bootstrap_key
 ## Fact that gets the ceph key "client.bootstrap-osd"
 
-Facter.add(:ceph_osd_bootstrap_key) do
+Facter.add(:ceph_admin_key) do
   setcode do
-    Facter::Util::Resolution.exec("ceph auth get-key client.bootstrap-osd")
+    Facter::Util::Resolution.exec("ceph auth get-key client.admin")
   end
 end
 
