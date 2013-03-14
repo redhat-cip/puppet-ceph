d2f4a1b1a78f0ca8b9c45c39f34fd92192571d3e
François Charlier <francois.charlier@enovance.com>
2013-03-14 14:15:21 +0100

    Update ceph::osd::device manifest

--- manifests/osd/device.pp
+++ manifests/osd/device.pp
@@ -20,57 +20,86 @@ define ceph::osd::device (
 ) {
 
   include ceph::osd
+  include ceph::conf
 
-  $devname = regsubst($name, /.*\//, '')
+  $devname = regsubst($name, '.*/', '')
 
-	exec { "mktable_gpt_${devname}":
-		command => "parted -a optimal --script ${name} mktable gpt",
-		unless => "parted --script ${name} print|grep -sq 'Partition Table: gpt'",
-		require => Package [ "parted" ]
-	}
+  exec { "mktable_gpt_${devname}":
+    command => "parted -a optimal --script ${name} mktable gpt",
+    unless => "parted --script ${name} print|grep -sq 'Partition Table: gpt'",
+    require => Package [ "parted" ]
+  }
 
-	exec { "mkpart_${devname}":
-		command => "parted -a optimal -s ${name} mkpart ceph 0% 100%",
-		unless => "parted ${name} print | egrep '^ 1.*ceph$'",
-		require => [ Package [ "parted" ], Exec [ "mktable_gpt_${name}" ] ]
-	}
+  exec { "mkpart_${devname}":
+    command => "parted -a optimal -s ${name} mkpart ceph 0% 100%",
+    unless => "parted ${name} print | egrep '^ 1.*ceph$'",
+    require => [ Package [ "parted" ], Exec [ "mktable_gpt_${devname}" ] ]
+  }
 
-	exec { "mkfs_${devname}":
-		command => "mkfs.xfs -f -d agcount=${::processorcount} -l size=1024m -n size=64k ${name}1",
-		require => [ Package [ "xfsprogs" ], Exec [ "mkpart_${name}"] ],
-	}
+  exec { "mkfs_${devname}":
+    command => "mkfs.xfs -f -d agcount=${::processorcount} -l size=1024m -n size=64k ${name}1",
+    unless  => "xfs_admin -l ${name}1",
+    require => [ Package [ "xfsprogs" ], Exec [ "mkpart_${devname}"] ],
+  }
+
+  $blkid_uuid_fact = "blkid_uuid_${devname}1"
+  $blkid = inline_template('<%= scope.lookupvar(blkid_uuid_fact) %>')
+
+  if $blkid != 'undefined' {
+    exec { "ceph_osd_create_${devname}":
+      command => "ceph osd create ${blkid}",
+      unless  => "ceph osd dump | grep -sq ${blkid}",
+    }
+  }
 
   $osd_id_fact = "ceph_osd_id_${devname}1"
   $osd_id = inline_template('<%= scope.lookupvar(osd_id_fact) %>')
 
-  if $osd_id {
-    mount { "${ceph::osd::osd_data}/osd.${osd_id}":
+  if $osd_id != 'undefined' {
+
+    ceph::conf::osd { $osd_id:
+      device       => $name,
+      cluster_addr => $ipaddress_eth1,
+      public_addr  => $ipaddress_eth0,
+    }
+
+    $osd_data = "${::ceph::osd::osd_data}/osd.${osd_id}"
+
+    file { $osd_data:
+      ensure => directory,
+    }
+
+    mount { $osd_data:
       ensure  => mounted,
       device  => "${name}1",
       atboot  => true,
       fstype  => 'xfs',
       options => 'rw,noatime,inode64',
       pass    => 2,
-      require => [ Exec [ "mkfs_${devname}" ] ]
+      require => [
+        Exec["mkfs_${devname}"],
+        File[$osd_data]
+      ],
     }
-#
-#    exec { "ceph-osd-mkfs-${::ceph_osd_id_name}":
-#      command => "\
-#ceph-osd -c /etc/ceph.conf \
-#-i ${::ceph_osd_id_name} \
-#--mkfs \
-#--mkkey \
-#--osd-uuid ${name}
-#--monmap /var/lib/ceph/tmp/monmap",
-#      creates => "${osd_data_expanded}/keyring",
+
+    exec { "ceph-osd-mkfs-${osd_id}":
+      command => "\
+ceph-osd -c /etc/ceph.conf \
+-i ${osd_id} \
+--mkfs \
+--mkkey",
+      creates => "${osd_data}/keyring",
 #      before  => [
 #        Exec['ceph-admin-key'],
 #        #Exec['ceph-osd-bootstrap-key'],
 #        Service["ceph-osd.${osd_id}"],
 #      ],
-#      require => [Anchor['key_ok'], Concat['/etc/ceph/ceph.conf']],
-#    }
-#
+      require => [
+        Mount[$osd_data],
+        Concat['/etc/ceph/ceph.conf'],
+      ],
+    }
+
 #    exec { "ceph-osd-register-${osd_id}":
 #      command => "\
 #ceph auth add osd.${osd_id} 'allow *' mon 'allow rwx' \
@@ -91,15 +120,3 @@ define ceph::osd::device (
   }
 
 }
-
-
-
-
-
-
-# Definition de la fonction
-define partitionning {
-}
-
-# Exec de la fonction
-partitionning { ["/dev/sdb", "/dev/sdc"]:}
