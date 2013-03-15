0b05e406c88dbf92cd0d90843710d37b63cbbb91
François Charlier <francois.charlier@enovance.com>
2013-03-15 13:56:50 +0100

    Try to fix osds being recreated whereas not needed

--- manifests/osd/device.pp
+++ manifests/osd/device.pp
@@ -43,79 +43,81 @@ define ceph::osd::device (
   }
 
   $blkid_uuid_fact = "blkid_uuid_${devname}1"
+  notify { "BLKID FACT ${devname}: ${blkid_uuid_fact}": }
   $blkid = inline_template('<%= scope.lookupvar(blkid_uuid_fact) %>')
+  notify { "BLKID ${devname}: ${blkid": }
 
   if $blkid != 'undefined' {
     exec { "ceph_osd_create_${devname}":
       command => "ceph osd create ${blkid}",
       unless  => "ceph osd dump | grep -sq ${blkid}",
     }
-  }
 
-  $osd_id_fact = "ceph_osd_id_${devname}1"
-  $osd_id = inline_template('<%= scope.lookupvar(osd_id_fact) %>')
+    $osd_id_fact = "ceph_osd_id_${devname}1"
+    $osd_id = inline_template('<%= scope.lookupvar(osd_id_fact) %>')
 
-  if $osd_id != 'undefined' {
+    if $osd_id != 'undefined' {
 
-    ceph::conf::osd { $osd_id:
-      device       => $name,
-      cluster_addr => $ipaddress_eth1,
-      public_addr  => $ipaddress_eth0,
+      ceph::conf::osd { $osd_id:
+        device       => $name,
+        cluster_addr => $ipaddress_eth1,
+        public_addr  => $ipaddress_eth0,
+      }
+
+      $osd_data = regsubst($::ceph::conf::osd_data, '\$id', $osd_id)
+
+      file { $osd_data:
+        ensure => directory,
+      }
+
+      mount { $osd_data:
+        ensure  => mounted,
+        device  => "${name}1",
+        atboot  => true,
+        fstype  => 'xfs',
+        options => 'rw,noatime,inode64',
+        pass    => 2,
+        require => [
+          Exec["mkfs_${devname}"],
+          File[$osd_data]
+          ],
+      }
+
+      exec { "ceph-osd-mkfs-${osd_id}":
+        command => "\
+        ceph-osd -c /etc/ceph/ceph.conf \
+        -i ${osd_id} \
+        --mkfs \
+        --mkkey",
+        creates => "${osd_data}/keyring",
+        require => [
+          Mount[$osd_data],
+          Concat['/etc/ceph/ceph.conf'],
+          ],
+      }
+
+      exec { "ceph-osd-register-${osd_id}":
+        command => "\
+        ceph auth add osd.${osd_id} osd 'allow *' mon 'allow rwx' \
+        -i ${osd_data}/keyring",
+        require => Exec["ceph-osd-mkfs-${osd_id}"],
+      }
+
+      exec { "ceph-osd-crush-${osd_id}":
+        command => "\
+        ceph osd crush set ${osd_id} 1 root=default host=${::hostname}",
+        require => Exec["ceph-osd-register-${osd_id}"],
+      }
+
+      service { "ceph-osd.${osd_id}":
+        ensure  => running,
+        start   => "service ceph start osd.${osd_id}",
+        stop    => "service ceph stop osd.${osd_id}",
+        status  => "service ceph status osd.${osd_id}",
+        require => Exec["ceph-osd-crush-${osd_id}"],
+      }
+
     }
-
-    $osd_data = regsubst($::ceph::conf::osd_data, '\$id', $osd_id)
-
-    file { $osd_data:
-      ensure => directory,
-    }
-
-    mount { $osd_data:
-      ensure  => mounted,
-      device  => "${name}1",
-      atboot  => true,
-      fstype  => 'xfs',
-      options => 'rw,noatime,inode64',
-      pass    => 2,
-      require => [
-        Exec["mkfs_${devname}"],
-        File[$osd_data]
-      ],
-    }
-
-    exec { "ceph-osd-mkfs-${osd_id}":
-      command => "\
-ceph-osd -c /etc/ceph/ceph.conf \
--i ${osd_id} \
---mkfs \
---mkkey",
-      creates => "${osd_data}/keyring",
-      require => [
-        Mount[$osd_data],
-        Concat['/etc/ceph/ceph.conf'],
-      ],
-    }
-
-    exec { "ceph-osd-register-${osd_id}":
-      command => "\
-ceph auth add osd.${osd_id} osd 'allow *' mon 'allow rwx' \
--i ${osd_data}/keyring",
-      require => Exec["ceph-osd-mkfs-${osd_id}"],
-    }
-
-    exec { "ceph-osd-crush-${osd_id}":
-      command => "\
-ceph osd crush set ${osd_id} 1 root=default host=${::hostname}",
-      require => Exec["ceph-osd-register-${osd_id}"],
-    }
-
-    service { "ceph-osd.${osd_id}":
-      ensure  => running,
-      start   => "service ceph start osd.${osd_id}",
-      stop    => "service ceph stop osd.${osd_id}",
-      status  => "service ceph status osd.${osd_id}",
-      require => Exec["ceph-osd-crush-${osd_id}"],
-    }
-
   }
 
 }
