24ae4c7b948ec1b6fbdbeb34db46b9278ef4d0a4
Dan van der Ster <daniel.vanderster@cern.ch>
2013-07-01 12:21:19 +0200

    add ceph yum repository

---
+++ manifests/yum/ceph.pp
@@ -0,0 +1,14 @@
+class ceph::yum::ceph (
+  $release = 'cuttlefish'
+) {
+  yumrepo { 'ceph':
+    descr    => "Ceph ${release} repository",
+    baseurl  => "http://ceph.com/rpm-${release}/el6/x86_64/",
+    gpgkey   =>
+      'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
+    gpgcheck => 1,
+    enabled  => 1,
+    priority => 5,
+    before   => Package['ceph'],
+  }
+}
