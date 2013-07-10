40e004796164aaee4410b596ba7f2b1483499e95
dotalton <dotalton@cisco.com>
2013-07-09 21:42:36 -0700

    added paths to additional files

--- manifests/key.pp
+++ manifests/key.pp
@@ -3,6 +3,10 @@ define ceph::key (
   $keyring_path = "/var/lib/ceph/tmp/${name}.keyring",
 ) {
 
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
+  }
+
   exec { "ceph-key-${name}":
     command => "ceph-authtool ${keyring_path} --create-keyring --name='client.${name}' --add-key='${secret}'",
     creates => $keyring_path,
