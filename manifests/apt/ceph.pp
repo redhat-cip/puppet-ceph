1ae8988f4fc5caa5a43e8991956b5d220610beb2
Kurt Huwig <k.huwig@telekom.de>
2013-08-02 08:42:19 +0200

    Allow to configure ceph's apt source

--- manifests/apt/ceph.pp
+++ manifests/apt/ceph.pp
@@ -1,13 +1,16 @@
 class ceph::apt::ceph (
-  $release = 'bobtail'
+  $release             = 'bobtail',
+  $apt_key_source      = 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
+  $apt_key_id          = '17ED316D',
+  $apt_source_location = "http://ceph.com/debian-${release}/"
 ) {
   apt::key { 'ceph':
-    key        => '17ED316D',
-    key_source => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
+    key        => $apt_key_id,
+    key_source => $apt_key_source,
   }
 
   apt::source { 'ceph':
-    location => "http://ceph.com/debian-${release}/",
+    location => $apt_source_location,
     release  => $::lsbdistcodename,
     require  => Apt::Key['ceph'],
     before   => Package['ceph'],
