9ee5c96588455b2c7a1049668f0be1102ddb9040
Kurt Huwig <k.huwig@telekom.de>
2013-08-05 11:06:14 +0200

    Avoid to use parameter release in the parameter apt_source_location

--- manifests/apt/ceph.pp
+++ manifests/apt/ceph.pp
@@ -2,15 +2,22 @@ class ceph::apt::ceph (
   $release             = 'bobtail',
   $apt_key_source      = 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
   $apt_key_id          = '17ED316D',
-  $apt_source_location = "http://ceph.com/debian-${release}/"
+  $apt_source_location = undef
 ) {
+
+  if (undef == $apt_source_location) {
+    $real_apt_source_location = "http://ceph.com/debian-${release}"
+  } else {
+    $real_apt_source_location = $apt_source_location
+  }
+
   apt::key { 'ceph':
     key        => $apt_key_id,
     key_source => $apt_key_source,
   }
 
   apt::source { 'ceph':
-    location => $apt_source_location,
+    location => $real_apt_source_location,
     release  => $::lsbdistcodename,
     require  => Apt::Key['ceph'],
     before   => Package['ceph'],
