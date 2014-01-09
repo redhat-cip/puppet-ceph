732b8a2d2c2a099507aaace439eba1a30e2f9d91
Sebastien Badia <sebastien.badia@enovance.com>
2014-01-09 11:57:27 +0100

    facter: fix ceph_keyring fact
    If ceph is installed but not configured

--- lib/facter/ceph_keyring.rb
+++ lib/facter/ceph_keyring.rb
@@ -6,8 +6,8 @@
 require 'facter'
 require 'json'
 
-raw_auth = Facter::Util::Resolution.exec("ceph auth list -f json")
-if !raw_auth.nil?
+if system("ceph -s > /dev/null 2>&1")
+  raw_auth = Facter::Util::Resolution.exec("ceph auth list -f json")
   json_auth = JSON.parse(raw_auth)
   json_auth['auth_dump'].each do |k|
     if k['entity'] =~ /client(.*)/
@@ -17,4 +17,4 @@ if !raw_auth.nil?
       end # if !client.admin
     end # match client
   end # all entity
-end # if data
+end # if system
