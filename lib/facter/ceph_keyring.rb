8f50fd518a384e9d9f04c87e4c748c187c281655
Leseb <sebastien.han@enovance.com>
2014-01-31 19:09:27 +0100

    Add timeout to prevent facter hanging issue
    Add timeout to prevent facter hanging issue

--- lib/facter/ceph_keyring.rb
+++ lib/facter/ceph_keyring.rb
@@ -6,16 +6,24 @@
 require 'facter'
 require 'json'
 
-# if ceph isn't configured => Error initializing cluster client: Error
-if system("ceph -s > /dev/null 2>&1")
-  raw_auth = %x(ceph auth list -f json)
-  json_auth = JSON.parse(raw_auth)
-  json_auth['auth_dump'].each do |k|
-    if k['entity'] =~ /client(.*)/
-      if k['entity'] != 'client.admin'
-        keyring = k['entity'].gsub!(/^client\./,'')
-        Facter.add("ceph_keyring_#{keyring}") { setcode { k['key'] } }
-      end # if !client.admin
-    end # match client
-  end # all entity
-end # if system
+timeout = 10
+
+begin
+  Timeout::timeout(timeout) {
+    # if ceph isn't configured => Error initializing cluster client: Error
+    if system("ceph -s > /dev/null 2>&1")
+      raw_auth = %x(ceph auth list -f json)
+      json_auth = JSON.parse(raw_auth)
+      json_auth['auth_dump'].each do |k|
+        if k['entity'] =~ /client(.*)/
+          if k['entity'] != 'client.admin'
+            keyring = k['entity'].gsub!(/^client\./,'')
+            Facter.add("ceph_keyring_#{keyring}") { setcode { k['key'] } }
+          end # if !client.admin
+        end # match client
+      end # all entity
+    end # if system
+  }
+rescue Timeout::Error
+  Facter.warnonce('ceph command timeout in ceph_keyring fact')
+end
