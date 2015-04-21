0db85aaa297373e5aa61684473c5aea21986b022
Risto Laurikainen <risto.laurikainen@csc.fi>
2015-04-21 11:35:09 +0300

    Use OS major release fact in yum repo URL
    Previously the el6 repo was always installed. Changed this so that the
    right URL is determined by $::operatingsystemmajrelease.

--- manifests/yum/ceph.pp
+++ manifests/yum/ceph.pp
@@ -23,7 +23,7 @@ class ceph::yum::ceph (
 ) {
   yumrepo { 'ceph':
     descr    => "Ceph ${release} repository",
-    baseurl  => "http://ceph.com/rpm-${release}/el6/x86_64/",
+    baseurl  => "http://ceph.com/rpm-${release}/el${::operatingsystemmajrelease}/x86_64/",
     gpgkey   =>
       'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
     gpgcheck => 1,
